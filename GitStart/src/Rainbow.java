5bc19612081d2d5335ad4a84f91a6f81d0464979
Артём <Swile93@gmail.com>
2020-03-01 14:06:12 +0200

    homework

---
+++ GitStart/src/Rainbow.java
@@ -0,0 +1,11 @@
+public class Rainbow
+{
+    public static void main(String[] args)
+    {
+        String[] rainbow = {"Красный", "Оранжевый", "Желтый", "Зеленый", "Голубой", "Синий", "Фиолетовый"};
+        for (int i = rainbow.length; i >= 1; i--)
+        {
+            System.out.println(rainbow[i]);
+        }
+    }
+}
