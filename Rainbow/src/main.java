80575e340023983338be376abb8946f4fdbd7dcb
Артём <Swile93@gmail.com>
2020-03-08 11:03:29 +0200

    FinishCommit

--- Rainbow/src/main.java
+++ Rainbow/src/main.java
@@ -1,11 +1,21 @@
 public class main
 {
     public static void main(String[] args) {
-        String text = "Красный Оранжевый Желтый Зеленый Голубой Синий Фиолетовый";
-        String [] colors = text.split("\\s+");
-        for (int i = colors.length - 1; i >= 0; i--)
-        {
+        String [] colors = {"Красный ", "Оранжевый ", "Желтый ", "Зеленый ", "Голубой ", "Синий ", "Фиолетовый "};
+
+        for (int i = 0; i >= colors.length; i++) {
             System.out.println(colors[i]);
+        }
+        int n = colors.length;
+        String temp;
+        for (int i = 0; i < n / 2; i++) {
+            temp = colors[n - i - 1];
+            colors[n - i - 1] = colors[i];
+            colors[i] = temp;
+        }
+        //Выводим конечный массив в консоль
+        for (int i = 0; i < colors.length; i++) {
+            System.out.print(colors[i]);
         }
     }
 }
