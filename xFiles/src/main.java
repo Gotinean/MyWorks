794282f3651f49e202a4a9dbd93755c4ec4d258f
Артём <Swile93@gmail.com>
2020-03-09 21:49:33 +0200

    NotFinalCommit

--- xFiles/src/main.java
+++ xFiles/src/main.java
@@ -1,29 +1,36 @@
+import java.util.Scanner;
+
 public class main
 {
     public static void main(String[] args)
     {
-        String [][] xFiles = new String[7][7];
+        System.out.println("Введите длину строки");
+        int valueOfX;
+        Scanner scanner = new Scanner(System.in);
+        valueOfX = scanner.nextInt();
+        String [][] xFiles = new String[valueOfX][valueOfX];
         int a;
         int b;
-        for(a = 0; a <= 6; a++)
-            for (b = 0; b <= 6; b++)
-                if (xFiles[a][b] != "Х")
+        for (a = 0; a <= xFiles.length -1; a++)
+            for (b = 0; b <= xFiles.length -1; b++)
+                if (a == b)
+                {
+                xFiles[a][b] = "X";
+                }
+        else if (b - a == xFiles.length)
+                {
+                    xFiles[a][b] = "X";
+                }
+        else if (b - a == valueOfX - valueOfX%2)
+                {xFiles[a][b] = "X";}
+        else if (a - b == valueOfX - valueOfX%2)
+                {xFiles[a][b] = "X";}
+        for(a = 0; a <= xFiles.length-1; a++)
+            for (b = 0; b <= xFiles.length-1; b++)
+                if (xFiles[a][b] != "X")
                 {
                     xFiles[a][b] = " ";
                 }
-        xFiles[0][0] = "Х";
-        xFiles[1][1] = "X";
-        xFiles[2][2] = "X";
-        xFiles[3][3] = "X";
-        xFiles[4][4] = "X";
-        xFiles[5][5] = "X";
-        xFiles[6][6] = "X";
-        xFiles[0][6] = "X";
-        xFiles[1][5] = "X";
-        xFiles[2][4] = "X";
-        xFiles[4][2] = "X";
-        xFiles[5][1] = "X";
-        xFiles[6][0] = "X";
         for (int i = 0; i < xFiles.length; i++) {  //идём по строкам
             for (int j = 0; j < xFiles[i].length; j++) {//идём по столбцам
                 System.out.print(" " + xFiles[i][j] + " "); //вывод элемента
