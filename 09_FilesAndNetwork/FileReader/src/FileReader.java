4cf41695702e9868132ce8e3c218a36a639d758c
Артём <Swile93@gmail.com>
2020-06-30 23:28:30 +0300

    FileReader

--- 09_FilesAndNetwork/FileReader/src/FileReader.java
+++ 09_FilesAndNetwork/FileReader/src/FileReader.java
@@ -2,10 +2,14 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class FileReader {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         while (true) {
             System.out.println("Введите путь до нужной директории");
             Scanner scanner = new Scanner(System.in);
@@ -13,41 +17,34 @@ public class FileReader {
             File folder = null;
             folder = new File(url);
             if (folder.exists()) {
-                long length = 0;
-                if (getFolderSize(folder) > 1024) {
-                    length = (getFolderSize(folder) / 1024);
-                    if (length > 1024) {
-                        length = length / 1024;
-                        if (length > 1024) {
-                            length = length / 1024;
-                            System.out.println("размер папки и вложенных файлов в сумме равен " + length + " ГБ");
-                        } else {
-                            System.out.println("размер папки и вложенных файлов в сумме равен " + length + " МБ");
-                        }
-                    } else {
-                        System.out.println("размер папки и вложенных файлов в сумме равен " + length + " КБ");
-                    }
-                } else {
-                    System.out.println(getFolderSize(folder) + " байт");
-                }
+                long length = getFolderSize(folder);
+                System.out.println(sizeConverter(length));
+
             } else {
                 System.out.println("Вы ввели неверный путь до директории, перепроверьте и попробуйте снова");
             }
         }
     }
 
-    private static long getFolderSize(File folder) {
-        long length = 0;
-        File[] files = folder.listFiles();
-        int count = files.length;
-
-        for (int i = 0; i < count; i++) {
-            if (files[i].isFile()) {
-                length += files[i].length();
-            } else {
-                length += getFolderSize(files[i]);
-            }
-        }
-        return length;
+    private static long getFolderSize(File folder) throws IOException {
+        Path fold = Paths.get(String.valueOf(folder));
+        long size = Files.walk(fold)
+                .filter(p -> p.toFile().isFile())
+                .mapToLong(p -> p.toFile().length())
+                .sum();
+        return size;
     }
+    private static String sizeConverter(long size){
+
+        if (size < 1024) return size + " Б";
+
+        int exp = (int) (Math.log(size) / (Math.log(1024)));
+
+        char unitsPrefix = "КМГТПЭ".charAt(exp - 1);
+
+        return String.format("%.2f %sБ", size / Math.pow(1024, exp), unitsPrefix);
+
+    }
+
+
 }
