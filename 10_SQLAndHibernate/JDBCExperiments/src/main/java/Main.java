1b9e01a7a21e4e217a02a219876c8c2c692231cc
arty <swile93@gmail.com>
2020-08-30 14:12:03 +0300

    JDBCExperiments

--- 10_SQLAndHibernate/JDBCExperiments/src/main/java/Main.java
+++ 10_SQLAndHibernate/JDBCExperiments/src/main/java/Main.java
@@ -1,22 +1,23 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String url = "jdbc:mysql://localhost:3306/skillbox";
-        String user = "root";
-        String pass = "insert_password";
-        Connection connection = DriverManager.getConnection(url,user,pass);
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SELECT name, COUNT(subscription_date)/MONTH(MAX(subscription_date)) FROM\n" +
-                "Courses JOIN Subscriptions ON Courses.id = Subscriptions.course_id GROUP BY Subscriptions.course_id;\n");
-        while (resultSet.next()){
-            String courseName = resultSet.getString(1);
-            String courseCount = resultSet.getString(2);
-            System.out.print(courseName + ":");
-            System.out.println(courseCount);
-        }
-        connection.close();
-        resultSet.close();
-        statement.close();
+       StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+               .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+       SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+       Session session = sessionFactory.openSession();
+
+       Course course = session.get(Course.class, 1);
+        System.out.println("Курсу " + course.getName() + " обучается " + course.getStudentsCount() + "студентов");
+       sessionFactory.close();
     }
 }
