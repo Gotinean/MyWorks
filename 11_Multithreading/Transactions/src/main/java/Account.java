a9f40a2fcf43321f9479628c92ed986988ca080c
swile <swile93@gmail.com>
2021-02-10 17:32:56 +0200

    Transaction

--- 11_Multithreading/Transactions/src/main/java/Account.java
+++ 11_Multithreading/Transactions/src/main/java/Account.java
@@ -9,15 +9,18 @@ public class Account
 {
     private long money;
     private String accNumber;
+    private boolean blocked;
+
+    public boolean isBlocked() {
+        return blocked;
+    }
 
     public static void makeAccounts(int accId, List<Account> list) throws InterruptedException {
-
         int a = 0;
         long amount = a + Math.round(Math.random() * 100000);
         String b = String.valueOf(accId);
-        Account account = new Account(amount, b);
+        Account account = new Account(amount, b, false);
         list.add(account);
-
     }
 
 
--- 11_Multithreading/Transactions/src/main/java/Bank.java
+++ 11_Multithreading/Transactions/src/main/java/Bank.java
@@ -1,17 +1,24 @@
+import lombok.SneakyThrows;
+import lombok.Synchronized;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
 
 public class Bank
 {
+    private final Random random = new Random();
+    private Bank bank = new Bank();
     private long totalAmount;
-
-
     public long getTotalAmount() {
         return totalAmount;
     }
+    private List<Account> list;
 
-    private final Random random = new Random();
+    public static void main(String[] args) throws InterruptedException {
+        new Bank().main();
+    }
 
     public synchronized boolean isFraud(Account fromAccount, Account toAccount, long amount)
         throws InterruptedException
@@ -19,7 +26,6 @@ public class Bank
         Thread.sleep(1000);
         return random.nextBoolean();
     }
-
     /**
      * TODO: реализовать метод. Метод переводит деньги между счетами.
      * Если сумма транзакции > 50000, то после совершения транзакции,
@@ -27,37 +33,39 @@ public class Bank
      * метод isFraud. Если возвращается true, то делается блокировка
      * счетов (как – на ваше усмотрение)
      */
-    public void transfer(Account fromAccount, Account toAccount, long amount) throws InterruptedException {
-        if(amount > 50000){
-            isFraud(fromAccount, toAccount, amount);
-            if (!isFraud(fromAccount, toAccount, amount)){
-                if(fromAccount.getMoney() > amount) {
+    public synchronized void transfer(Account fromAccount, Account toAccount, long amount) throws InterruptedException {
+        if (fromAccount.isBlocked() | toAccount.isBlocked()) {
+            System.out.println("Транзакции невозможны. Счет одного из участников заблокирован," +
+                    " обратитесь в центральный офис банка");
+        } else {
+            if (amount > 50000) {
+                isFraud(fromAccount, toAccount, amount);
+                if (!isFraud(fromAccount, toAccount, amount)) {
+                    if (fromAccount.getMoney() > amount) {
+                        fromAccount.setMoney(fromAccount.getMoney() - amount);
+                        toAccount.setMoney(toAccount.getMoney() + amount);
+                        Thread.sleep(1005);
+                    } else {
+                        fromAccount.setBlocked(true);
+                        toAccount.setBlocked(true);
+                        System.out.println("У вас недостаточно средств на счету для осуществления транзакции \n " +
+                                "Доступная сумма: " + fromAccount.getMoney() + " \n Запрашиваемый перевод: " + amount);
+                    }
+                } else {
+                    System.out.println("Данная транзакция показалась нам подозрительной, \n" +
+                            " пожалуйста обратитесь в отделение банка для осуществления перевода");
+                }
+            } else {
+                if (fromAccount.getMoney() > amount) {
                     fromAccount.setMoney(fromAccount.getMoney() - amount);
                     toAccount.setMoney(toAccount.getMoney() + amount);
-                    Thread.sleep(1005);
-                }
-                else {
+                } else {
                     System.out.println("У вас недостаточно средств на счету для осуществления транзакции \n " +
                             "Доступная сумма: " + fromAccount.getMoney() + " \n Запрашиваемый перевод: " + amount);
                 }
             }
-            else {
-                System.out.println("Данная транзакция показалась нам подозрительной, \n" +
-                        " пожалуйста обратитесь в отделение банка для осуществления перевода");
-            }
-        }
-        else {
-            if(fromAccount.getMoney() > amount) {
-                fromAccount.setMoney(fromAccount.getMoney() - amount);
-                toAccount.setMoney(toAccount.getMoney() + amount);
-            }
-            else {
-                System.out.println("У вас недостаточно средств на счету для осуществления транзакции \n " +
-                        "Доступная сумма: " + fromAccount.getMoney() + " \n Запрашиваемый перевод: " + amount);
-            }
         }
     }
-
     /**
      * TODO: реализовать метод. Возвращает остаток на счёте.
      */
@@ -68,6 +76,50 @@ public class Bank
             balance = list.get(i).getMoney() + balance;
         }
         return balance;
+    }
+    public synchronized void makeAccounts(int accId, List<Account> list) throws InterruptedException {
+        int a = 0;
+        long amount = random.nextInt(200000);
+        String b = String.valueOf(accId);
+        Account account = new Account(amount, b, false);
+        list.add(account);
+
+    }
+    public synchronized void work(Bank bank) throws InterruptedException {
+        int firstAccount = random.nextInt(10);
+        int secondAccount = random.nextInt(10);
+        long transferMoney = random.nextInt(70000);
+        if (firstAccount != secondAccount) {
+            bank.transfer(list.get(firstAccount), list.get(secondAccount), transferMoney);
+            System.out.println(bank.getBalance(list));
+        }
+    }
+
+    public void main() throws InterruptedException {
+        long before = System.currentTimeMillis();
+        List<Thread> threads = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            int finalI = i;
+            threads.add(new Thread(()-> {
+                try {
+                    makeAccounts(finalI,list);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                for (int j = 0; j < 1000; j++) {
+                    try {
+                        work(bank);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }));
+        }
+        threads.forEach(Thread::start);
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
     }
 
 }
