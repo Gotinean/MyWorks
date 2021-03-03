370637754e7abcf5cad506a042bed8e4250c915f
swile <swile93@gmail.com>
2021-03-03 20:08:41 +0200

    SiteMap

--- 11_Multithreading/SiteMapGenerator/src/main/java/Sitemap.java
+++ 11_Multithreading/SiteMapGenerator/src/main/java/Sitemap.java
@@ -8,75 +8,51 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.RecursiveTask;
 
 public class Sitemap {
     static Set<String> links = new TreeSet<>();
 
     static String path = "https://skillbox.ru/";
-    static int numOfOperations;
     static int numOfThreads = Runtime.getRuntime().availableProcessors();
+    static Elements elements;
     public static void main(String[] args) throws IOException, InterruptedException {
-        getLinks(path, links);
+        links.add(path);
+        int numOfOperations = links.size();
+        //getLinks(path, links);
+        ForkJoinPool pool = new ForkJoinPool(numOfThreads);
+        pool.invoke(new MyFork(0,numOfOperations));
         PrintWriter pw = new PrintWriter("file.txt");
-        for(String link : links) {
-            int countNulls=0;
-            for (char element : link.toCharArray()){
-                if (element == '/') countNulls++;
+        for (String link : links) {
+            if (!link.endsWith("/")) {
+                link = link + "/";
             }
-            if (countNulls == 3) {
-                pw.write(link + "\n");
+            int count = 0;
+            StringBuilder ident = new StringBuilder();
+            for (char element : link.toCharArray()) {
+                if (element == '/') count++;
             }
-            else if(countNulls == 4 && link.endsWith("/")){
-                pw.write("  "+link + "\n");
+            if (count > 3) {
+                ident.append("  ".repeat(count - 3));
             }
-            else if(countNulls == 4 && Character.isLetter(link.charAt(link.length()-1))){
-                pw.write("      "+link + "\n");
-            }
-            else if(countNulls == 5){
-                pw.write("      "+link + "\n");
-            }
-            else if(countNulls == 6){
-                pw.write("        "+link + "\n");
-            }
+            pw.write(ident + link + "\n");
         }
         pw.close();
-//        ForkJoinPool pool = new ForkJoinPool(numOfThreads);
-//        readyBlocks = pool.invoke(new MyFork(0, numOfOperations));
-//        //getSitemap(links2);
-//        File file = new File("./files/file.txt");
-//        PrintWriter pw = new PrintWriter(file);
-//        for (String string : readyBlocks) {
-//            pw.write(string + "\n");
-//  //          System.out.println(string);
-//        }
-//        pw.close();
     }
-
-
     static void getLinks(String url, Set<String> urls) throws InterruptedException {
     Thread.sleep(500);
     if (urls.contains(url)) {
         return;
     }
     urls.add(url);
-
     try {
-        Document doc = Jsoup.connect(url).get();
-        Elements elements = doc.select("a");
-        for(Element element : elements){
-            if(urls.contains(element.absUrl("href"))){
+        Document doc = Jsoup.connect(path).get();
+        elements = doc.select("a");
+        for (Element element : elements) {
+            if (!check(element.absUrl("href"), urls)) {
                 continue;
             }
-            if(element.absUrl("href").equals("https://skillbox.ru/privacy_policy.pdf")) {
-                continue;
-            }
-            if(!element.absUrl("href").matches("https:\\/\\/skillbox\\.ru[a-zA-Z\\/-]+")){
-                continue;
-            }
-//            if(element.absUrl("href").length() < url.length()){
-//                continue;
-//            }
             System.out.println(element.absUrl("href"));
             urls.add(url);
             getLinks(element.absUrl("href"), urls);
@@ -84,50 +60,39 @@ public class Sitemap {
     } catch (IOException e) {
         e.printStackTrace();
     }
-}
     }
+    static boolean check(String path, Set<String> urls) {
+        if (path.endsWith(".pdf") | !path.matches("https:\\/\\/skillbox\\.ru[a-zA-Z\\/-]+") |
+                urls.contains(path)) {
+            return false;
+        } else
+            return true;
 
-//    static class MyFork extends RecursiveTask<List<String>> {
-//        int from, to;
-//        public MyFork(int from, int to){
-//            this.from = from;
-//            this.to = to;
-//        }
-//
-//        @Override
-//        protected List<String> compute() {
-//            try {
-//                Thread.sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            if((to - from) <= numOfOperations){
-//                Document document = null;
-//                try {
-//                    document = Jsoup.connect(path).get();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//                assert document != null;
-//                Element content = document.getElementById("sidebar");
-//                Elements links2 = content.select("li");
-//                try {
-//                    getSitemap(links2);
-//                } catch (IOException | InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//
-//            }
-//            else {
-//                int middle = (to + from)/2;
-//                MyFork firstHalf = new MyFork(from,middle);
-//                firstHalf.fork();
-//                MyFork secondHalf = new MyFork(middle+1, to);
-//                List<String> secondValue = secondHalf.compute();
-//                firstHalf.join();
-//                return secondValue;
-//            }
-//            return null;
-//        }
-//    }
-//}
+    }
+    static class MyFork extends RecursiveTask<Set<String>>{
+        int from, to;
+        public MyFork(int from, int to){
+            this.from = from;
+            this.to = to;
+        }
+        @Override
+        protected Set<String> compute() {
+            if((to-from) <= links.size()/numOfThreads){
+                try {
+                    getLinks(path, links);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                int middle = (to+from)/2;
+                MyFork firstHalf = new MyFork(from, middle);
+                firstHalf.fork();
+                MyFork secondHalf = new MyFork(middle+1, to);
+                links = secondHalf.compute();
+                secondHalf.join();
+                return links;
+            }
+            return links;
+        }
+    }
+}
