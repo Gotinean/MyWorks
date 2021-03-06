0a003aae7c9160dd668ba18cbd4227a5c301d679
swile <swile93@gmail.com>
2021-03-06 13:12:24 +0200

    SiteMap

--- 11_Multithreading/SiteMapGenerator/src/main/java/Sitemap.java
+++ 11_Multithreading/SiteMapGenerator/src/main/java/Sitemap.java
@@ -3,12 +3,10 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.RecursiveAction;
 import java.util.concurrent.RecursiveTask;
 
 public class Sitemap {
@@ -19,10 +17,8 @@ public class Sitemap {
     static Elements elements;
     public static void main(String[] args) throws IOException, InterruptedException {
         links.add(path);
-        int numOfOperations = links.size();
-        //getLinks(path, links);
         ForkJoinPool pool = new ForkJoinPool(numOfThreads);
-        pool.invoke(new MyFork(0,numOfOperations));
+        links.addAll(pool.invoke(new MyFork(path)));
         PrintWriter pw = new PrintWriter("file.txt");
         for (String link : links) {
             if (!link.endsWith("/")) {
@@ -40,59 +36,62 @@ public class Sitemap {
         }
         pw.close();
     }
-    static void getLinks(String url, Set<String> urls) throws InterruptedException {
-    Thread.sleep(500);
-    if (urls.contains(url)) {
-        return;
-    }
-    urls.add(url);
-    try {
-        Document doc = Jsoup.connect(path).get();
-        elements = doc.select("a");
-        for (Element element : elements) {
-            if (!check(element.absUrl("href"), urls)) {
-                continue;
-            }
-            System.out.println(element.absUrl("href"));
-            urls.add(url);
-            getLinks(element.absUrl("href"), urls);
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    }
-    static boolean check(String path, Set<String> urls) {
-        if (path.endsWith(".pdf") | !path.matches("https:\\/\\/skillbox\\.ru[a-zA-Z\\/-]+") |
-                urls.contains(path)) {
-            return false;
-        } else
-            return true;
 
-    }
     static class MyFork extends RecursiveTask<Set<String>>{
-        int from, to;
-        public MyFork(int from, int to){
-            this.from = from;
-            this.to = to;
+        String url;
+        public MyFork(String url) {
+            this.url = url;
         }
+        Set<String> result = new TreeSet<>();
+
+        static Set<String> getLinks(String url, Set<String> urls) throws InterruptedException {
+            Thread.sleep(500);
+            if (urls.contains(url)) {
+                return urls;
+            }
+            urls.add(url);
+            try {
+                Document doc = Jsoup.connect(path).get();
+                elements = doc.select("a");
+                for (Element element : elements) {
+                    if (!check(element.absUrl("href"), urls)) {
+                        continue;
+                    }
+                    System.out.println(element.absUrl("href"));
+                    urls.add(url);
+                    getLinks(element.absUrl("href"), urls);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return urls;
+        }
+        static boolean check(String path, Set<String> urls) {
+            if (path.endsWith(".pdf") | !path.matches("https:\\/\\/skillbox\\.ru[a-zA-Z\\/-]+") |
+                    urls.contains(path)) {
+                return false;
+            } else
+                return true;
+
+        }
+
         @Override
         protected Set<String> compute() {
-            if((to-from) <= links.size()/numOfThreads){
-                try {
-                    getLinks(path, links);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            result.add(url);
+            try {
+                Set<String> linksInsideCompute = getLinks(url, result);
+                List<MyFork> taskList = new ArrayList<>();
+                for(String link : linksInsideCompute){
+                    MyFork myFork = new MyFork(link);
+                    myFork.fork();
+                    taskList.add(myFork);
                 }
-            } else {
-                int middle = (to+from)/2;
-                MyFork firstHalf = new MyFork(from, middle);
-                firstHalf.fork();
-                MyFork secondHalf = new MyFork(middle+1, to);
-                links = secondHalf.compute();
-                secondHalf.join();
-                return links;
+                taskList.forEach(MyFork::join);
+                return result;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            return links;
+            return result;
         }
     }
 }
