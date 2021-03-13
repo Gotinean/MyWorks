bc947e1ecf4dbc2866800a51df5da36cf2bda389
swile <swile93@gmail.com>
2021-03-13 14:53:24 +0200

    SiteMap

--- 11_Multithreading/SiteMapGenerator/src/main/java/Sitemap.java
+++ 11_Multithreading/SiteMapGenerator/src/main/java/Sitemap.java
@@ -10,10 +10,9 @@ import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
 
 public class Sitemap {
-    static Set<String> links = new TreeSet<>();
     public static void main(String[] args) throws IOException, InterruptedException {
         String path = "https://skillbox.ru/";
-        links = new ForkJoinPool().invoke(new MyFork(path));
+        Set<String> links = new ForkJoinPool().invoke(new MyFork(path));
         PrintWriter pw = new PrintWriter("file.txt");
         for (String link : links) {
             if (!link.endsWith("/")) {
@@ -39,31 +38,28 @@ public class Sitemap {
         @Override
         protected Set<String> compute() {
             List<MyFork> taskList = new ArrayList<>();
-            Set<String> result = getLinks();
+            Set<String> result = getLinks(url);
             for (String s : result) {
-                if(check(s)) {
-                    taskList.add((MyFork) new MyFork(s).fork());
+                taskList.add((MyFork) new MyFork(s).fork());
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                     }
                 }
-            }
-            for (MyFork child : taskList) {
-                child.join();
-                result.add(child.url);
-            }
+            taskList.forEach(child -> result.addAll(child.join()));
             return result;
         }
-        private Set<String> getLinks(){
+        private Set<String> getLinks(String url){
             Set<String> urls = new TreeSet<>();
             try {
                 Document doc = Jsoup.connect(url).get();
-                Elements elements = doc.select("a");
+                Elements elements = doc.select("a[href]");
                 for (Element element : elements) {
-                    String url = element.absUrl("href");
-                    urls.add(url);
-                    System.out.println(url);
+                    String link = element.absUrl("href");
+                    if(check(link, url, urls)) {
+                        urls.add(link);
+                        System.out.println(link);
+                    }
 
                 }
             } catch (IOException e) {
@@ -71,8 +67,8 @@ public class Sitemap {
             }
             return urls;
         }
-        boolean check(String url) {
-            return (url.matches("https:\\/\\/skillbox\\.ru[a-zA-Z\\/-]+") && url.endsWith("/"));
+        boolean check(String url, String link, Set<String> urls) {
+            return (url.startsWith(link) && url.endsWith("/") && !urls.contains(url) && !url.equals(link));
         }
     }
 }
